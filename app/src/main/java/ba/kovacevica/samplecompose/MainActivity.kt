7b1bcc470900113df641ad60a41588427c9a60ce
Adnan Kovačević <k9.adnan@gmail.com>
2022-08-23 16:22:48 +0200

    Initital commit

---
+++ app/src/main/java/ba/kovacevica/samplecompose/MainActivity.kt
@@ -0,0 +1,67 @@
+package ba.kovacevica.samplecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.rememberCoroutineScope
+import ba.kovacevica.samplecompose.ui.theme.SampleComposeTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SampleComposeTheme {
+                val scaffoldState = rememberScaffoldState()
+                val scope = rememberCoroutineScope()
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        AppBar(
+                            onNavigationIconClick = {
+                                scope.launch {
+                                    scaffoldState.drawerState.open()
+                                }
+                            }
+                        )
+                    },
+                    drawerContent = {
+                        DrawerHeader()
+                        DrwarerBody(
+                            items = listOf(
+                                MenuItem(
+                                    id = "home",
+                                    title = "Home",
+                                    contentDescription = "Go to home screen",
+                                    icon = Icons.Default.Home
+                                ),
+                                MenuItem(
+                                    id = "settings",
+                                    title = "Settings",
+                                    contentDescription = "Go to settings screen",
+                                    icon = Icons.Default.Settings
+                                ),
+
+                                MenuItem(
+                                    id = "help",
+                                    title = "Help",
+                                    contentDescription = "Need Help?",
+                                    icon = Icons.Default.Info
+                                ),
+
+                                )
+                        ) {
+                            println("Clicked on ${it.title}")
+                        }
+                    }) {
+
+                }
+            }
+        }
+    }
+}
